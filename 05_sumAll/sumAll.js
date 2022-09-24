ce50de809e78f4219d31ec1d71f97681310c8e4c
Christian <cmanalo.io@outlook.com>
2022-09-23 20:33:01 -0700

    Sum of numbers given the minimum and maximum number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    
+   if (!Number.isInteger(min) || !Number.isInteger(max) ) {
+    return "ERROR"
+   }
+   if ((min <0) || (max < 0)){
+    return "ERROR"}
+
+   if(min > max){
+    let temp = min
+    min = max
+    max = temp 
+
+   }
+
+   sum = 0;
+   for(i=min;i<max+1;i++ ){
+    sum += i
+   }
+
+   return sum;
+
 
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
