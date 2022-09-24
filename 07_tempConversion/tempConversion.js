d5d3cdc4943899dd13a3f25359cb6e5ec5363a2a
Christian <cmanalo.io@outlook.com>
2022-09-24 01:28:58 -0700

    Create a temperature converter

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,17 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {
+
+  let cel = (fahrenheit - 32) * (5/9)
+  return Number(cel.toFixed(1))
+};
+
+const ctof = function(celsius) {
+
+let fah = celsius * (9/5) + 32
+return Number(fah.toFixed(1))
+
 
 };
 
-const ctof = function() {
-
-};
 
 // Do not edit below this line
 module.exports = {
